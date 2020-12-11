26a92582cc85d5dc2bcac43f58e555319fbc3a1b
Ryan Scott <rscott@bestcompany.com>
2020-12-11 08:10:59 -0700

    Remove test data for day 10

--- day_10/day10.py
+++ day_10/day10.py
@@ -1,11 +1,6 @@
 with open('day_10/joltage_adaptors.txt') as f:
     adaptors = [int(x) for x in f.readlines()]
 
-adaptors = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
-
-adaptors = [28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11,
-            1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3]
-
 adaptors.sort()
 
 # part 1
